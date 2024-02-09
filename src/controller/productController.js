4efe2a97098f4ec57441ed52c0a9c96e35a3465a
rafaelziraldo <rafazira83@gmail.com>
2024-02-08 21:07:39 -0300

    d

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -12,9 +12,7 @@ const { promisePool } = require('../../dbConection/db');
 
   exports.getAllProduct = async (req, res) => {
     try {
-      const [rows, fields] = await promisePool.query('select * from Product p inner join Supplier s on p.SupplierId=s.Id inner join Category c on c.Id=p.CategoryId');
-      console.log(rows)
-      console.log(fields)
+      const [rows, fields] = await promisePool.query('select * from Product p inner join Supplier s on p.SupplierId=s.Id inner join Category c on c.Id=p.CategoryId');     
       res.status(200).json(rows);
     } catch (error) {
       console.error('Error en la consulta a la base de datos:', error.message);
